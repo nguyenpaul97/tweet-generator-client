d45702f6af8ba629e3d7be59e997b6daf94714cf
nguyenpaul97 <nguyenpaul.ic.1997@gmail.com>
2020-08-01 13:47:12 -0700

    Pushing build

---
+++ build/precache-manifest.ad8c73d350d56229a5c96ce53828892e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a487a4d6dd2143359d8c4fa621191f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26ad6d14e7669988d2e4",
+    "url": "/static/css/main.746b1bf8.chunk.css"
+  },
+  {
+    "revision": "928078fad1f5512d4dbc",
+    "url": "/static/js/2.6a6abfd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a6abfd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26ad6d14e7669988d2e4",
+    "url": "/static/js/main.40f18e50.chunk.js"
+  },
+  {
+    "revision": "7a4a4e5162642c6bffb2",
+    "url": "/static/js/runtime-main.07c98560.js"
+  },
+  {
+    "revision": "b07e12ff81258aae9c9450f43fd1e913",
+    "url": "/static/media/trump.b07e12ff.jpg"
+  },
+  {
+    "revision": "d1b3e6567cd0274dacf0daf6e54cdb7c",
+    "url": "/static/media/twitter-verification.d1b3e656.png"
+  }
+]);
